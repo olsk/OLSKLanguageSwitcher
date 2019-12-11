c8682233bf33977e28d652a2188537a64b53fd7e
Rosano Coutinho <pub@xeari.com>
2019-12-11 16:37:50 -0500

    Stub routes

---
+++ controller.js
@@ -0,0 +1,23 @@
+exports.OLSKControllerRoutes = function() {
+	return [{
+		OLSKRoutePath: '/stub/OLSKLanguageSwitcherEJS',
+		OLSKRouteMethod: 'get',
+		OLSKRouteFunction (req, res, next) {
+			return res.render(require('path').join(__dirname, 'stub-view'), {
+				OLSKLanguageSwitcherStubParams: req.query,
+			});
+		},
+		OLSKRouteSignature: 'OLSKLanguageSwitcherEJSStubRoute',
+		OLSKRouteLanguages: ['en', 'fr', 'es'],
+		OLSKRouteIsHidden: process.env.NODE_ENV === 'production',
+	}, {
+		OLSKRoutePath: '/stub/OLSKLanguageSwitcherSvelte',
+		OLSKRouteMethod: 'get',
+		OLSKRouteFunction (req, res, next) {
+			return res.render(require('path').join(__dirname, 'stub-view'));
+		},
+		OLSKRouteSignature: 'OLSKLanguageSwitcherSvelteStubRoute',
+		OLSKRouteLanguages: ['en', 'fr', 'es'],
+		OLSKRouteIsHidden: process.env.NODE_ENV === 'production',
+	}];
+};
