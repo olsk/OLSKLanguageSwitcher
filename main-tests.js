0d5388fc15bd796cdf5c8c118d6fdb083f03f3ba
Rosano Coutinho <pub@xeari.com>
2020-12-02 11:09:32 -0300

    Spec OLSKLanguageSwitcherRecipes

---
+++ main-tests.js
@@ -0,0 +1,244 @@
+const { throws, deepEqual } = require('assert');
+
+const mod = require('./main.js');
+
+const uWindow = function (inputData = {}) {
+	return Object.assign({
+		location: {},
+	}, inputData);
+};
+
+const uLocalized = function (inputData, OLSKLanguageSwitcherVersionName) {
+	if (inputData === 'OLSKLanguageSwitcherVersionName') {
+		return {
+			[OLSKLanguageSwitcherVersionName]: OLSKLanguageSwitcherVersionName,
+		};
+	}
+	return inputData + '-LOCALIZED';
+};
+
+const uFormatted = function () {
+	return JSON.stringify(Array.from(arguments)) + '-FORMATTED';
+};
+
+const uCanonical = function (param1, param2) {
+	return param1 + '-CANONICAL-' + param2.OLSKRoutingLanguage;
+};
+
+const uRandomElement = function (inputData) {
+	return inputData[Date.now() % inputData.length];
+};
+
+describe('OLSKLanguageSwitcherLauncherFakeItemProxy', function test_OLSKLanguageSwitcherLauncherFakeItemProxy() {
+
+	it('returns object', function () {
+		const item = mod.OLSKLanguageSwitcherLauncherFakeItemProxy();
+		deepEqual(item, {
+			LCHRecipeName: 'OLSKLanguageSwitcherLauncherFakeItemProxy',
+			LCHRecipeCallback: item.LCHRecipeCallback,
+		});
+	});
+
+	context('LCHRecipeCallback', function () {
+		
+		it('returns undefined', function () {
+			deepEqual(mod.OLSKLanguageSwitcherLauncherFakeItemProxy().LCHRecipeCallback(), undefined);
+		});
+
+	});
+
+});
+
+describe('OLSKLanguageSwitcherLauncherItemSwitch', function test_OLSKLanguageSwitcherLauncherItemSwitch() {
+
+	const _OLSKLanguageSwitcherLauncherItemSwitch = function (inputData = {}) {
+		return mod.OLSKLanguageSwitcherLauncherItemSwitch(Object.assign({
+			ParamWindow: uWindow(),
+			OLSKLocalized: uLocalized,
+			OLSKFormatted: uFormatted,
+			ParamLanguageCode: Math.random().toString(),
+			ParamRouteConstant: Math.random().toString(),
+			OLSKCanonicalFor: uCanonical,
+		}, inputData))
+	}
+
+	it('throws if not object', function () {
+		throws(function () {
+			mod.OLSKLanguageSwitcherLauncherItemSwitch(null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if ParamWindow not window', function () {
+		throws(function () {
+			_OLSKLanguageSwitcherLauncherItemSwitch({
+				ParamWindow: {},
+			});
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if OLSKLocalized not function', function () {
+		throws(function () {
+			_OLSKLanguageSwitcherLauncherItemSwitch({
+				OLSKLocalized: null,
+			});
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if OLSKFormatted not function', function () {
+		throws(function () {
+			_OLSKLanguageSwitcherLauncherItemSwitch({
+				OLSKFormatted: null,
+			});
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if ParamLanguageCode not string', function () {
+		throws(function () {
+			_OLSKLanguageSwitcherLauncherItemSwitch({
+				ParamLanguageCode: null,
+			});
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if ParamRouteConstant not string', function () {
+		throws(function () {
+			_OLSKLanguageSwitcherLauncherItemSwitch({
+				ParamRouteConstant: null,
+			});
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if OLSKCanonicalFor not function', function () {
+		throws(function () {
+			_OLSKLanguageSwitcherLauncherItemSwitch({
+				OLSKCanonicalFor: null,
+			});
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns object', function () {
+		const ParamLanguageCode = Math.random().toString();
+		const item = _OLSKLanguageSwitcherLauncherItemSwitch({
+			ParamLanguageCode,
+		});
+
+		deepEqual(item, {
+			LCHRecipeSignature: [
+				'OLSKLanguageSwitcherLauncherItemSwitch',
+				ParamLanguageCode,
+			].join('-'),
+			LCHRecipeName: uFormatted(uLocalized('OLSKLanguageSwitcherVersionFormat'), uLocalized('OLSKLanguageSwitcherVersionName', ParamLanguageCode)[ParamLanguageCode]),
+			LCHRecipeCallback: item.LCHRecipeCallback,
+			LCHRecipeIsExcluded: item.LCHRecipeIsExcluded,
+		});
+	});
+
+	context('LCHRecipeCallback', function () {
+
+		it('sets ParamWindow.location', function () {
+			const ParamWindow = uWindow({
+				location: {},
+			});
+			const ParamRouteConstant = Math.random().toString();
+			const ParamLanguageCode = Math.random().toString();
+			
+			_OLSKLanguageSwitcherLauncherItemSwitch({
+				ParamWindow,
+				ParamLanguageCode,
+				ParamRouteConstant,
+			}).LCHRecipeCallback();
+
+			deepEqual(ParamWindow.location.href, uCanonical(ParamRouteConstant, {
+				OLSKRoutingLanguage: ParamLanguageCode,
+			}));
+		});
+
+	});
+
+});
+
+describe('OLSKLanguageSwitcherRecipes', function test_OLSKLanguageSwitcherRecipes() {
+
+	const _OLSKLanguageSwitcherRecipes = function (inputData = {}) {
+		return mod.OLSKLanguageSwitcherRecipes(Object.assign({
+			ParamLanguageCodes: [],
+			ParamCurrentLanguage: Math.random().toString(),
+			ParamLanguageCode: Math.random().toString(),
+			ParamSpecUI: false,
+			ParamWindow: uWindow(),
+			OLSKLocalized: uLocalized,
+			ParamLanguageCode: Math.random().toString(),
+			ParamRouteConstant: Math.random().toString(),
+			OLSKFormatted: uFormatted,
+			OLSKCanonicalFor: uCanonical,
+		}, inputData))
+	};
+
+	it('throws if not object', function () {
+		throws(function () {
+			mod.OLSKLanguageSwitcherRecipes(null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if ParamLanguageCodes not array', function () {
+		throws(function () {
+			_OLSKLanguageSwitcherRecipes({
+				ParamLanguageCodes: null,
+			});
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if ParamCurrentLanguage not string', function () {
+		throws(function () {
+			_OLSKLanguageSwitcherRecipes({
+				ParamCurrentLanguage: null,
+			});
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if ParamSpecUI not boolean', function () {
+		throws(function () {
+			_OLSKLanguageSwitcherRecipes({
+				ParamSpecUI: null,
+			});
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns producton recipes', function () {
+		const ParamLanguageCodes = Array.from(Array(Math.max(Date.now() % 10, 1))).map(function () {
+			return Math.random().toString();
+		});
+		const ParamCurrentLanguage = uRandomElement(ParamLanguageCodes);
+
+		deepEqual(_OLSKLanguageSwitcherRecipes({
+			ParamLanguageCodes,
+			ParamCurrentLanguage,
+		}).map(function (e) {
+			return e.LCHRecipeSignature || e.LCHRecipeName;
+		}), ParamLanguageCodes.reduce(function (coll, item) {
+			if (item === ParamCurrentLanguage) {
+				return coll;
+			}
+
+			return coll.concat([
+				'OLSKLanguageSwitcherLauncherItemSwitch',
+				item,
+			].join('-'));
+		}, []));
+	});
+
+	context('OLSK_IS_TESTING_BEHAVIOUR', function () {
+
+		it('returns all recipes if true', function () {
+			deepEqual(_OLSKLanguageSwitcherRecipes({
+				ParamSpecUI: true,
+			}).map(function (e) {
+				return e.LCHRecipeSignature || e.LCHRecipeName;
+			}), Object.keys(mod).filter(function (e) {
+				return e.match(/Fake/);
+			}));
+		});
+	
+	});
+
+});
---
+++ main.js
@@ -0,0 +1,91 @@
+const mod = {
+
+	OLSKLanguageSwitcherLauncherFakeItemProxy () {
+		return {
+			LCHRecipeName: 'OLSKLanguageSwitcherLauncherFakeItemProxy',
+			LCHRecipeCallback () {},
+		};
+	},
+
+	OLSKLanguageSwitcherLauncherItemSwitch (params) {
+		if (typeof params !== 'object' || params === null) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (!params.ParamWindow.location) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+		
+		if (typeof params.OLSKLocalized !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof params.OLSKFormatted !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof params.ParamLanguageCode !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof params.ParamRouteConstant !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof params.OLSKCanonicalFor !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		const _this = this;
+
+		return {
+			LCHRecipeSignature: ['OLSKLanguageSwitcherLauncherItemSwitch', params.ParamLanguageCode].join('-'),
+			LCHRecipeName: params.OLSKFormatted(params.OLSKLocalized('OLSKLanguageSwitcherVersionFormat'), params.OLSKLocalized('OLSKLanguageSwitcherVersionName', params.ParamLanguageCode)[params.ParamLanguageCode]),
+			LCHRecipeCallback () {
+				params.ParamWindow.location.href = params.OLSKCanonicalFor(params.ParamRouteConstant, {
+					OLSKRoutingLanguage: params.ParamLanguageCode,
+				});
+			},
+			LCHRecipeIsExcluded () {
+				return !!params.ParamAuthorized;
+			},
+		};
+	},
+
+	OLSKLanguageSwitcherRecipes (params) {
+		if (typeof params !== 'object' || params === null) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (!Array.isArray(params.ParamLanguageCodes)) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof params.ParamCurrentLanguage !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof params.ParamSpecUI !== 'boolean') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return [
+			mod.OLSKLanguageSwitcherLauncherFakeItemProxy(),
+		].concat(params.ParamLanguageCodes.filter(function (e) {
+			return e !== params.ParamCurrentLanguage;
+		}).map(function (ParamLanguageCode) {
+			return mod.OLSKLanguageSwitcherLauncherItemSwitch(Object.assign(params, {
+				ParamLanguageCode,
+			}))
+		})).filter(function (e) {
+			if (params.ParamSpecUI) {
+				return true;
+			}
+
+			return !(e.LCHRecipeSignature || e.LCHRecipeName).match(/Fake/);
+		});
+	},
+
+};
+
+Object.assign(exports, mod);
